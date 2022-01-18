4fa661d6de7d18a9be4cd9f824d5107ca2e17fce
Alain Tavan <alain57@gmail.com>
2022-01-18 19:14:36 +0100

    change colors and add values for better readings

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import datetime
-import streamlit as st
 import numpy as np
 import pandas as pd
+from datetime import date
 import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import seaborn as sns
+import os
 from urllib.request import urlretrieve
+import streamlit as st
+
+
 
 url = "https://data.drees.solidarites-sante.gouv.fr/api/records/1.0/download/?dataset=covid-19-resultats-par-age-issus-des-appariements-entre-si-vic-si-dep-et-vac-si"
 url2 = "https://data.drees.solidarites-sante.gouv.fr/explore/dataset/covid-19-resultats-par-age-issus-des-appariements-entre-si-vic-si-dep-et-vac-si/information/"
@@ -32,10 +37,12 @@ def transform_age(s):
     return d[s]
 
 
-@st.cache(show_spinner=False)
 def get_data():
-    urlretrieve(url, filename="data.csv")
-    df = pd.read_csv("data.csv", sep=";", on_bad_lines="skip")
+    today = date.today()
+    date_string = today.strftime("%d-%m")
+    today_csv = "data-" + date_string + ".csv" 
+    retrieve_csv(today_csv)
+    df = pd.read_csv(today_csv, sep=";", on_bad_lines="skip")
 
     # Keep only the last 15 days available
     dates_kept = sorted(list(set(df["date"])))[-15:]
@@ -43,13 +50,18 @@ def get_data():
 
     return df, dates_kept
 
+def retrieve_csv(today_csv):
+    files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith(".csv") and f != today_csv]
+    for f in files:
+        os.remove(f)
+    if not os.path.isfile(today_csv):  
+        urlretrieve(url, filename=today_csv)    
 
 df_initial, dates_kept = get_data()
 earliest, latest = dates_kept[0], dates_kept[-1]
 earliest = f"{earliest[-2:]}/{earliest[5:7]}"
 latest = f"{latest[-2:]}/{latest[5:7]}"
 
-@st.cache(show_spinner=False)
 def preprocess_data(df_initial):
     # Relabel the vaccination status and the age columns
     df = df_initial.copy()
@@ -83,23 +95,23 @@ def preprocess_data(df_initial):
 df = preprocess_data(df_initial)
 
 titles = {
-    0:  r"$\bf{Véritables\ Hospitalisations}$"
+    0:  "$\\bf{Véritables\ Hospitalisations}$"
         + f" du {earliest} au {latest}",
-    1:  r"$\bf{Véritables\ entrées\ en\ soins\ critiques}$"
+    1:  "$\\bf{Véritables\ entrées\ en\ soins\ critiques}$"
         + f" du {earliest} au {latest}",
-    2: r"$\bf{Vériables\ décès}$" + f" du {earliest} au {latest}",
+    2:  "$\\bf{Vériables\ décès}$" + f" du {earliest} au {latest}",
 
-    3:  r"$\bf{Hospitalisations\ simulation\ sur\ 1\ million}$"
+    3:  "$\\bf{Hospitalisations\ simulation\ sur\ 1\ million}$"
         + f" du {earliest} au {latest}",
-    4:  r"$\bf{Entrées\ en\ soins\ critiques\ simulation\ sur\ 1\ million}$"
+    4:  "$\\bf{Entrées\ en\ soins\ critiques\ simulation\ sur\ 1\ million}$"
         + f" du {earliest} au {latest}",
-    5: r"$\bf{Décès\ simulation\ sur\ 1\ million}$" + f" du {earliest} au {latest}",
+    5:  "$\\bf{Décès\ simulation\ sur\ 1\ million}$" + f" du {earliest} au {latest}",
 
-    6:  r"$\bf{Hospitalisations\ simulation\ sur\ 10\ millions}$"
+    6:  "$\\bf{Hospitalisations\ simulation\ sur\ 10\ millions}$"
         + f" du {earliest} au {latest}",
-    7:  r"$\bf{Entrées\ en\ soins\ critiques\ simulation\ sur\ 10\ millions}$"
+    7:  "$\\bf{Entrées\ en\ soins\ critiques\ simulation\ sur\ 10\ millions}$"
         + f" du {earliest} au {latest}",
-    8: r"$\bf{Décès\ simulation\ sur\ 10\ millions}$" + f" du {earliest} au {latest}",
+    8:  "$\\bf{Décès\ simulation\ sur\ 10\ millions}$" + f" du {earliest} au {latest}",
 }
 
 
@@ -107,23 +119,41 @@ titles = {
 def create_fig(key, title):
     sns.set_context("paper")
     fig, ax = plt.subplots(figsize=(8, 4))
-    sns.barplot(
+    colors = ["lightgreen", "lightcoral"]
+    g=sns.barplot(
         data=df,
         x="age",
         y=key,
         hue="vac_statut",
-        palette=sns.color_palette("rocket"),
         ax=ax,
     )
+    sns.set_palette(sns.color_palette(colors))
     sns.despine()
-    handles, labels = ax.get_legend_handles_labels()
-    ax.legend(handles, [x[5:] for x in labels], title=legend_title)
     ax.set(
         ylabel=yaxis_title, xlabel=xaxis_title, title=title,
-    )
+     )
+     
+    handles, labels = ax.get_legend_handles_labels()
+    
+    ax.legend(handles, [x[5:] for x in labels], title=legend_title)
+    
+    show_values_on_bars(ax)
     return fig
 
 
+def show_values_on_bars(axs):
+    def _show_on_single_plot(ax):        
+        for p in ax.patches:
+            _x = p.get_x() + p.get_width() / 2
+            _y = p.get_y() + p.get_height()
+            value = '{:.0f}'.format(p.get_height())
+            ax.text(_x, _y, value, ha="center", color='black') 
+
+    if isinstance(axs, np.ndarray):
+        for idx, ax in np.ndenumerate(axs):
+            _show_on_single_plot(ax)
+    else:
+        _show_on_single_plot(axs)
 
 st.sidebar.header("COVID-19 : cas graves en fonction de l'âge et du statut vaccinal en France")
 st.sidebar.markdown(f"""Les graphiques de cette page montrent les chiffres réel VS les simulations trompeuse des médias.
@@ -159,7 +189,10 @@ st.markdown(
             flex-direction: row;
         }
     </style>
-# La réalité sur le terrain""",
+# La réalité sur le terrain
+attention hospitaliations = personne qui se retrouve a l'hopital et qui a un test PCR positif!
+un enfant qui se casse un bras, qui n'a aucun symptome et qui est testé positif finira dans ce tableau.
+""",
     unsafe_allow_html=True,
 )
 
@@ -184,9 +217,16 @@ for key, title in [
     ("mort_per_1M", titles[5]),
 ]:st.pyplot(create_fig(key, title))
 
+
+
+
 st.markdown(
     """# L'arnaque médiatique sur 10 million d'habitant
 chiffres qui permettent à un président d'emmerdé VRAIMENT d'honnêtes citoyens!
+    
+    
+    
+    
     """,
     unsafe_allow_html=True,
 )
